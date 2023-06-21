9491d8801a0b945b7dd9f77c4fe6223be0efa952
pkuasule <pkuasule@users.noreply.github.com>
2023-06-21 15:52:57 +0800

    adjust photo height

--- layouts/Title-Photo-Desc-2Col.vue
+++ layouts/Title-Photo-Desc-2Col.vue
@@ -4,11 +4,11 @@
       <slot />
       <div class="grid grid-cols-2 gap-6">
         <div class="flex flex-col items-left">
-          <img class="w-full h-screen-60 object-cover object-center mt-4 bg-shade" :src="pic1">
+          <img class="w-full h-screen-30 object-cover object-center mt-4 bg-shade" :src="pic1">
           <slot name="C1" />
         </div>
         <div class="flex flex-col items-left">
-          <img class="w-full h-screen-60 object-cover object-center mt-4 bg-shade" :src="pic2">
+          <img class="w-full h-screen-30 object-cover object-center mt-4 bg-shade" :src="pic2">
           <slot name="C2" />
         </div>
       </div>
